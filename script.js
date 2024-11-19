7d265dc267f72ff4f80fb8eee2bcc7a2869685a1
Syedmasood-git <masoodhussaini46@gmail.com>
2024-11-19 14:03:02 +0530

    made some changes in play video

--- script.js
+++ script.js
@@ -51,10 +51,11 @@ function getViews(views) {
   }
 }
 function navigateTovideoDetails(videoId){
-  document.cookie=`id=${videoId.statistics[0].id}; path=play-video.html`;
+  document.cookie=`id=${videoId.statistics[0].id}; path=/play-video.html`;
   const baseURL = `${window.location.origin}/play-video.html`;
   window.location.href = baseURL;
 }
+console.log("Windiw",window.location.origin)
 function renderVideosOntoUI(videosList){
   console.log(videosList);
     videosList.forEach((video)=>{
